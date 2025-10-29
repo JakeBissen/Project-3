17b9198550b3f454e002d15ffb20b234def1ff21
JakeBissen <Joy303jake303@gmail.com>
2025-10-29 16:09:41 -0500

    moving on to adding a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import Contact from './Components/Contact';
 import TodoList from './Components/TodoList';
+import Homepage from './Components/Homepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
+
 function App() {
-  console.log("App component rendered");
 
   return (
     <>
-    <header>
-      <h1 className="text-center m-4 container">Welcome to the Task Manager App</h1>
-      
-       
-    </header>
-   <div className="">
-        <div className="row justify-content-center">
-        <div className="col-12 col-lg-6 text-center mb-4 col-12 col-md-8">
+     
     <BrowserRouter >
  <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand" to='/' >Task Manager</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -42,16 +36,17 @@ function App() {
   </div>
 </nav>
      <Routes> 
-        <Route path="/" element={<h1>Home Page</h1>}  />
+      <Route path="/" element={<Homepage />}  />
       <Route path="/todos" element={<TodoList/>}  />
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/Opt-todos" element={<TodoList/>}/>
     </Routes>
 </BrowserRouter>
-        </div>
-        </div>
-      </div>
-       
+     
+
+   
+      
+    
     </>
   )
 }
---
+++ src/Components/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Homepage() {
+  return (
+    <div className="container mt-5">
+        <h2 className="mb-4">Homepage</h2>
+        <p>Welcome to the Task Manager App! Use the navigation bar to access your to-do list and contact form.</p>
+
+        
+    </div>
+  );
+}
+
+export default Homepage;
