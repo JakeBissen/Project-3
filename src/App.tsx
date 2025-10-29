2adec4d719827815aac8bef5927858b7bc43f0b5
JakeBissen <Joy303jake303@gmail.com>
2025-10-29 10:43:59 -0500

    reworking on navbar, setting up a basic functional one.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Contact from './Components/contact';
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import Contact from './Components/Contact';
 import TodoList from './Components/TodoList';
-import NavbarMenu from './Components/NAvbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 function App() {
@@ -9,20 +11,47 @@ function App() {
 
   return (
     <>
-    <nav>
-    <NavbarMenu />
+    <header>
+      <h1 className="text-center m-4 container">Welcome to the Task Manager App</h1>
+      
+       
+    </header>
+   <div className="">
+        <div className="row justify-content-center">
+        <div className="col-12 col-lg-6 text-center mb-4 col-12 col-md-8">
+    <BrowserRouter >
+ <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Navbar</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/todos">ToDos</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/contact">Contact Form</Link>
+        </li>
+     <li className="nav-item">
+          <Link className="nav-link" to="/Opt-todos">Optional Assignments</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
      <Routes> 
         <Route path="/" element={<h1>Home Page</h1>}  />
       <Route path="/todos" element={<TodoList/>}  />
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/Opt-todos" element={<TodoList/>}/>
     </Routes>
-</nav>
+</BrowserRouter>
+        </div>
+        </div>
+      </div>
        
-       <div>
-        <Contact />
-       </div>
-
     </>
   )
 }
--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -8,12 +8,7 @@ const TodoList = () => {
   return(
 
     <div>
-      <h2>Todo List Component</h2>
-      <ul>
-        <li>Todo Item 1</li>
-        <li>Todo Item 2</li>
-        <li>Todo Item 3</li>
-      </ul>
+      
     </div>
   )
 
